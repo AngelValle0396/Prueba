4bc459e8bd779ab0d661a392c997962ad78b162b
AngelValle0396 <66800307+AngelValle0396@users.noreply.github.com>
2020-07-23 18:14:21 -0500

    Commit
    CuartoCommit

---
+++ app/src/main/java/com/example/banderas_lc1/Galeria.java
@@ -0,0 +1,33 @@
+package com.example.banderas_lc1;
+
+import android.graphics.drawable.Drawable;
+
+import android.widget.ImageView;
+
+
+import com.mindorks.placeholderview.Animation;
+import com.mindorks.placeholderview.annotations.Animate;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+
+@Animate(Animation.CARD_LEFT_IN_DESC)
+@NonReusable
+@Layout(R.layout.galeria_ly)
+public class Galeria {
+
+    @View(R.id.imageView)
+    private ImageView imageView;
+
+    private Drawable mDrawable;
+
+    public Galeria(Drawable drawable) {
+        mDrawable = drawable;
+    }
+
+    @Resolve
+    private void onResolved() {
+        imageView.setImageDrawable(mDrawable);
+    }
+}
