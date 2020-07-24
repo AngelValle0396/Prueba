2a581e5a01eafc80b5d7c6bff38616b45b6d18cd
AngelValle0396 <66800307+AngelValle0396@users.noreply.github.com>
2020-07-23 19:45:55 -0500

    commit
    OctavoCommit

---
+++ app/src/main/java/com/example/banderas_lc1/Pais.java
@@ -0,0 +1,33 @@
+package com.example.banderas_lc1;
+
+public class Pais {
+    private String name;
+
+    private String alpha2Code;
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlpha2Code() {
+        return alpha2Code;
+    }
+
+    public void setAlpha2Code(String alpha2Code) {
+        this.alpha2Code = alpha2Code;
+    }
+}
